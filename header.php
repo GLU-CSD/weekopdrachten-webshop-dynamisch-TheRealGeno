0d8e619999d7d8a687565ea0ccff377366093574
TheRealGeno <genokooijman@gmail.com>
2024-03-14 13:48:26 +0100

    first commit

---
+++ header.php
@@ -0,0 +1,23 @@
+<div id="container">
+        <div class="taal"> Uw taal: </div>
+        <h2> HYPERX </h2>
+        <div class="account"> Account </div>
+        <div class="account"> <a href="/pages/factuur.html" class="cartButton"> Shopping Cart </a></div>
+        <div class="account"> <a href="/pages/login.html" class="cartButton">Login</a></div>
+    </div>
+
+    <div id="topbar">
+        <div class="logo"><img src="img/hyperxlogo.png" alt="logo" width="200" height="50"></div>
+        <div class="producten">
+            <button type="button" class="btn">Shop By Category</button>
+            <button type="button" class="btn">Shop By Platform</button>
+            <button type="button" class="btn">We're All Gamers</button>
+            <button type="button" class="btn">NGENUITY</button>
+            <button type="button" class="btn">Support</button>
+            <div class="search">
+                <form action="/action_page.php"></form>
+                <input type="text" id="fname" name="firstname" placeholder="Search..">
+                <button>Search </button>
+            </div>
+        </div>
+    </div>
