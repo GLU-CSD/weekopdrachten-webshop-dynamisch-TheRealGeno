17993c5afc730a99e5909174b7fbfa489846e048
TheRealGeno <genokooijman@gmail.com>
2024-03-22 11:03:45 +0100

    small images product info page

--- productinfo.php
+++ productinfo.php
@@ -34,14 +34,14 @@
     <div class="flex">
     <div id="productinfo">
         <div class="productphoto">
-          <img src=<?php echo $product["imageSrc"] ?> alt="mainImage" width="400" height="400"  >
+          <img src=<?php echo $product["imageSrc"] ?> id="mainImage" width="400" height="400"  >
             </div>
         <div class="productinformation">
             <div class="producttitel"> <?php  echo $product["name"]  ?></div>
             <div class="containerphoto">
-            <div class="productphoto1">check</div>
-            <div class="productphoto1">check</div>
-            <div class="productphoto1">check</div>
+            <div class="productphoto1"><img onclick="document.getElementById('mainImage').src=this.src" src=<?php echo $product["imageSrc"] ?> alt="mainImage" width="100" height="100"  ></div>
+            <div class="productphoto1"><img onclick="document.getElementById('mainImage').src=this.src" src=<?php echo $product["imageSrc2"] ?> alt="mainImage" width="100" height="100"  ></div>
+            <div class="productphoto1"><img onclick="document.getElementById('mainImage').src=this.src" src=<?php echo $product["imageSrc1"] ?> alt="mainImage" width="90" height="90"  ></div>
             </div>
             <div class="price"> <?php echo $product["price"]?></div>
             <div class="producttext"> <?php echo $product["info"] ?></div>
--- shoppingcart.php
+++ shoppingcart.php
@@ -1,3 +1,10 @@
+<?php
+ session_start();
+?>
+
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,6 +12,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Shoppingcart</title>
     <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/shoppingcart.css">
 </head>
 <body>
     <header>
@@ -12,10 +20,21 @@
         include_once "header.php";
       ?>
     </header>
-       
-    
-
+  <div class="container1">  
+   <div id="cartContainer">
+     <div class="productimages">
+      hallo
+     </div>
+     <div class="cartInfo">
+        <div class="productName">hallo</div>
+        <div class="productPrice">hallo</div>
+     </div>
+   </div>
+  </div>
     <footer>
+    <div class="blackbar">
+    <div class="logo2"><img src="img/hyperxlogonobackground.png" alt="logo" width="200" height="50"></div>
+</div>
       <?php
        include_once "footer.php"
       ?>
