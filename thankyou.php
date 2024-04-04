5bfdaa6837afbc74ef3f699dd70e5e2fa4de6543
TheRealGeno <genokooijman@gmail.com>
2024-04-04 16:11:33 +0200

    end 4-4-2024

---
+++ thankyou.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="css/thankyou.css">
+</head>
+<body>
+
+    <?php include_once "header.php"; include "products.php"?>
+
+    <div class="thankscontainer">
+        <div class="thankyou">
+          <div class="titel"> Thank you for the purchase</div>
+          <div class="thankyouinfo">Items purchased : </div>
+          <div class="continueshopping"><a href="index.php"></a></div>
+        </div>
+    </div>
+
+    <?php include_once "footer.php"; ?>
+</body>
+</html>
